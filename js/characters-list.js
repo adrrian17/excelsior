0b594a0d56613bba9a39afcf0df11825b32aabaa
Adrian Ayala <adrian.ayala17@gmail.com>
2015-02-03 15:13:20 -0600

    Add a new method to generate a grid of characters
    This will use  the chunk method from lodash to split the entire array
    into chunks of 4 characters and then generate a row with each chunk

--- js/characters-list.js
+++ js/characters-list.js
@@ -1,15 +1,30 @@
 var Character = require('./character');
 var React = require('react');
+var chunk = require('lodash/array/chunk');
 
 var CharactersList = React.createClass({
+  getCharacters: function() {
+    var items = chunk(this.props.items, 4)
+
+    return (
+      <div>
+        {items.map(function(item, index) {
+          return (
+            <div className='row'>
+              {item.map(function(char, index) {
+                return <Character character={char}/>;
+              })}
+            </div>
+          )
+        })}
+      </div>
+    )
+  },
+
   render: function () {
     return (
       <div>
-        {
-          this.props.items.map(function(item, index) {
-            return <Character key={index} character={item}/>;
-          })
-        }
+        {this.getCharacters()}
       </div>
     );
   }
