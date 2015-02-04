cee3c2fbfe52924e7e6cce0b578bb86f1862f3bb
Adrian Ayala <adrian.ayala17@gmail.com>
2015-02-03 22:13:41 -0600

    Add the ExcelsiorApp component
    This component will be in charge of rendering the navbar and the grid,
    and in the future also the pagination links

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-var CharactersGrid = require('./characters-grid');
+var ExcelsiorApp = require('./app');
 var React = require('react');
-var request = require('superagent');
 
-// Marvel public API key
-require('./api-key');
-
-var baseUrl = 'http://gateway.marvel.com'
-var charactersUrl = baseUrl+'/v1/public/characters?'+key
-
-request.get(charactersUrl, function(res) {
-  var items = res.body.data.results;
-
-  React.render(<CharactersGrid items={items}/>, document.getElementById('content'));
-});
+React.render(<ExcelsiorApp/>, document.getElementById('content'));
