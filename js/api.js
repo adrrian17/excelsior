762d1a35ab9a28e6c085f592df427931d9be3a17
Adrian Ayala <adrian.ayala17@gmail.com>
2015-02-07 23:08:16 -0600

    Simplify characters URL in the API module

--- js/api.js
+++ js/api.js
@@ -4,14 +4,14 @@ var Q = require('q');
 // Marvel public API key
 require('./api-key');
 
-var baseUrl = 'http://gateway.marvel.com'
-var charactersUrl = baseUrl+'/v1/public/characters?'+key
+var charactersUrl = 'http://gateway.marvel.com/v1/public/characters'
 
 exports.getCharacters = function(offset) {
   var deferred = Q.defer();
 
   request
     .get(charactersUrl)
+    .query({ apikey: key })
     .query({ offset: offset })
     .end(function(res) {
       deferred.resolve(res.body.data);
