42d3261f014e8ab93ccbfbce3a5e8e0251cf23a4
Adrian Ayala <adrian.ayala17@gmail.com>
2015-02-10 20:09:46 -0600

    Fix typo in React require of the index route file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ require('node-jsx').install();
 
 var express   = require('express');
 var router    = express.Router();
-var React     = require('React');
+var React     = require('react');
 var Excelsior = React.createFactory(require('../components/app'));
 
 router.get('/', function(req, res) {
