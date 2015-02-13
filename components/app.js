7ba400a705edc8f99fb8eba64e78badd14491bd1
Adrian Ayala <adrian.ayala17@gmail.com>
2015-02-12 21:48:35 -0600

    Add a spinner component to indicate loading state

--- components/app.js
+++ components/app.js
@@ -1,27 +1,34 @@
+var React          = require('react');
+var API            = require('./api');
 var CharactersGrid = require('./characters-grid');
-var NavBar = require('./navbar');
-var Pager = require('./pager');
-var React = require('react');
-var API = require('./api');
+var NavBar         = require('./navbar');
+var Pager          = require('./pager');
+var Spinner        = require('./spinner');
 
 var ExcelsiorApp = React.createClass({
   getInitialState: function() {
     return {
       items: [],
       currentPage: 0,
-      offset: 0
+      offset: 0,
+      loaded: false
     };
   },
 
   componentDidMount: function() {
     API.getCharacters().then(function(res) {
-      this.setState({ items: res.results });
+      this.setState({
+        items: res.results,
+        loaded: true
+      });
     }.bind(this));
   },
 
   handlePageRequest: function(page) {
+    this.setState({ loaded: false });
+
     var currentPage = this.state.currentPage;
-    var offset;
+    var offset = 0;
 
     if(page > currentPage) {
       offset = this.state.offset + 20;
@@ -33,18 +40,26 @@ var ExcelsiorApp = React.createClass({
       this.setState({
         items: res.results,
         currentPage: page,
-        offset: offset
+        offset: offset,
+        loaded: true
       });
     }.bind(this));
   },
 
   render: function() {
+    var hide = this.state.loaded ? '' : 'hidden';
+
     return (
       <div>
         <NavBar/>
         <div className='container'>
-          <CharactersGrid items={this.state.items}/>
-          <Pager page={this.state.currentPage} changePage={this.handlePageRequest}/>
+          <Spinner loaded={this.state.loaded}/>
+          <div className={hide}>
+            <CharactersGrid items={this.state.items}/>
+            <Pager
+              page={this.state.currentPage}
+              changePage={this.handlePageRequest}/>
+          </div>
         </div>
       </div>
     );
