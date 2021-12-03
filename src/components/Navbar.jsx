1aefd4b58c5932da8dbd120e724b1648e25b334f
Adrian Ayala <adrian.ayala17@gmail.com>
2021-12-02 18:35:04 -0600

    Remove some unnecessary CSS and HTML from the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,17 @@ import { GoRepo } from 'react-icons/go';
 
 export default function Navbar() {
   return (
-    <nav className="flex fixed w-full items-center px-6 py-4 justify-between text-red-700 z-10">
-      <div className="flex items-center">
-        <span id="logo">Excelsior</span>
-      </div>
-      <div className="flex items-center">
-        <a
-          target="_blank"
-          href="https://github.com/adrrian17/excelsior"
-          rel="noopener noreferrer"
-          className="github-repo"
-        >
-          <GoRepo className="mr-2" />
-          Repo
-        </a>
-      </div>
+    <nav className="flex items-center justify-between px-12 py-4 bg-red-700 text-white">
+      <span id="logo">Excelsior</span>
+      <a
+        target="_blank"
+        href="https://github.com/adrrian17/excelsior"
+        rel="noopener noreferrer"
+        className="github-repo"
+      >
+        <GoRepo className="mr-2" />
+        Repo
+      </a>
     </nav>
   );
 }
